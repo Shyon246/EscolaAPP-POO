26d292c7f074c81e240a8762d07ca3d330ebaa4f
Milton Shyon de Lima Gurgel <milton.shyon@academico.ifrn.edu.br>
2023-01-13 11:32:07 -0300

    Adicionar arquivos de projeto.

---
+++ EscolaAPP/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscolaAPP
+{
+    public class Aluno
+    {
+        public int Id {get; set; }
+        public string Nome {get; set; }
+        public string Matricula { get; set; }
+        public string Email { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} - {Nome} - {Matricula} - {Email}";
+        }
+    }
+}
